0848fc1d9af8838670d5a203feb1ee357725dc3b
Valkinsenn <49046029+Valkinsenn@users.noreply.github.com>
2020-10-25 15:54:13 -0500

    Updated the Index page a bit and also added the initial version of a play-pause music player to the navbar.

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+// Runs Strict Mode
+"run strict";
+
+// Global Variables
+const pageAudio = document.querySelector("#track");
+const controlButton = document.querySelector("#playPause");
+
+
+// Functions
+window.onload = function() {
+    pageAudio.volume = 0.15;
+}
+
+function playPause() {
+    if (!pageAudio.paused) {
+        pageAudio.pause();
+        controlButton.className = "play";
+    } else {
+        pageAudio.play();
+        controlButton.className = "pause";
+    }
+}
